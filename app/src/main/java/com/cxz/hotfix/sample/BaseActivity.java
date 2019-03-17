d842c693f50d00b4a6e90e1d7837045ae520bfc7
chenxz <563859095@qq.com>
2019-03-17 22:39:47 +0800

    commit

---
+++ app/src/main/java/com/cxz/hotfix/sample/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.cxz.hotfix.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author chenxz
+ * @date 2019/3/17
+ * @desc
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/cxz/hotfix/sample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.cxz.hotfix.sample;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 运行时权限申请
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            if (checkSelfPermission(perms[0]) == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(perms, 1000);
+            }
+        }
+
+    }
+
+    public void jump(View view) {
+        startActivity(new Intent(this, SecondActivity.class));
+    }
+
+}
